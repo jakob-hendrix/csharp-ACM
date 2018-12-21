18ff6dbecab2c598b5b58873a78e2ce3a49fb907
p00293412 <jakob.hendrix@la.gov>
2018-12-21 14:08:42 -0600

    Current progress...implementing properties

---
+++ AMC.BL/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AMC.BL
+{
+    public class Customer
+    {
+        private string _lastName;
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public string FirstName { get; set; };
+
+        public string EmailAddress { get; set; }
+
+        public int CustomerId { get; private set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{LastName}, {FirstName}";
+            }
+        }
+
+    }
+}
